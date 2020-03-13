65e123404fa1ed7712c679a344ae20176e7d0b16
Kim Seung Eon <kim.se@gsshop.com>
2020-03-14 04:16:17 +0900

    Coding the UI part 2 - Input

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform} from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView} from 'react-native';
+import { render } from 'react-dom';
+import ToDo from "./ToDo";
 
 const { height, width } = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content"/>
-      <Text style={styles.title}>To Do</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}></TextInput>
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+
+  render() {
+    const { newToDo } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} 
+                     placeholder={"New To Do"} 
+                     value={newToDo} 
+                     onChangeText={this._controlNewToDo} 
+                     placeholderTextColor={"#999"} 
+                     returnKeyType={"done"} 
+                     autoCorrect={false} />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -51,6 +76,9 @@ const styles = StyleSheet.create({
     })
   },
   input: {
-
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
   },
 });
---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class ToDo extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>I'm ToDo!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
